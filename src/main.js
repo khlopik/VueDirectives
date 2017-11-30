4dc27335f14b89b3f6aab177589c0ef9faf32932
Oleksii Khlopotov <khlopik@gmail.com>
2017-11-30 14:45:28 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = binding.value;
+    let delay = binding.modifiers['delayed'] ? 3000 : 0;
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
